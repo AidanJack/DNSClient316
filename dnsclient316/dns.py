18a543f05369605f379f09a4d3ec5c89ba6a4cc9
Aidan Jackson <aidan.jackson@mail.mcgill.ca>
2022-02-10 10:45:52 -0500

    Created seperate dns class for static query formation

--- dnsclient316/dns.py
+++ dnsclient316/dns.py
@@ -1,4 +1,101 @@
+import random
 class DNS:
     @staticmethod
-    def example():
-        return None
+    def generatePacketID():
+        headerID = bytearray()
+        for i in range(2):
+            r = random.randint(0, 255)
+            headerID.append(r)
+        return headerID
+
+    @staticmethod
+    def generateQueryFlags():
+        flags = bytearray()
+        flags.append(1)
+        flags.append(0)
+        return flags
+
+    @staticmethod
+    def generateQDCount():
+        qCount = bytearray()
+        qCount.append(0)
+        qCount.append(1)
+        return qCount
+
+    @staticmethod
+    def generateANCount():
+        aCount = bytearray()
+        aCount.append(0)
+        aCount.append(0)
+        return aCount
+
+    @staticmethod
+    def generateNSCount():
+        nCount = bytearray()
+        nCount.append(0)
+        nCount.append(0)
+        return nCount
+
+    @staticmethod
+    def generateARCount():
+        aCount = bytearray()
+        aCount.append(0)
+        aCount.append(0)
+        return aCount
+
+    @staticmethod
+    def generateNRCount():
+        nrCount = bytearray()
+        nrCount.append(0)
+        nrCount.append(0)
+        return nrCount
+
+    @staticmethod
+    # Generates  and returns a formatted DNS request Header.
+    def generateDNSHeader():
+        header = bytearray()
+        header.append(DNS.generatePacketID())
+        header.extend(DNS.generateQueryFlags())  # For us it should always be 1, and 0
+        header.extend(DNS.generateQDCount())  # QDCount is always 1 for us
+        header.extend(DNS.generateANCount())  # Default 0, only regarded in response packets
+        header.extend(DNS.generateNSCount())
+        header.extend(DNS.generateNRCount())
+        return header
+
+    @staticmethod
+    def generateQName(name):
+        qName = bytearray()
+        tokens = name.split('.')
+        for token in tokens:
+            token = token.replace(" ", "")
+            qName.append(len(token))
+            for char in token:
+                qName.append(char)
+        qName.append(0)
+        return qName
+
+    @staticmethod
+    def generateQType():
+        qType = bytearray()  # needs to choose between 1, 2, and 3. for now use 1 since default ip type.
+        qType.append(0)
+        qType.append(1)
+        return qType
+
+    @staticmethod
+    def generateQClass():
+        qClass = bytearray()
+        qClass.append(0)
+        qClass.append(1)
+        return qClass
+
+    @staticmethod
+    def generateDNSQuestions(name):
+        questions = bytearray()
+        questions.extend(DNS.generateQName(name))
+        questions.extend(DNS.generateQType())
+        questions.extend(DNS.generateQClass())
+        return questions
+
+    @staticmethod
+    def sendDNSRequest():
+        print("Sent")
--- dnsclient316/dnsClient.py
+++ dnsclient316/dnsClient.py
@@ -4,6 +4,7 @@ import sys
 import threading
 import argparse
 import random
+from dns import DNS
 parser = argparse.ArgumentParser()
 
 
@@ -21,98 +22,13 @@ def addTerminalArgs():
     parser.add_argument("-mx", "-nx", default="ip", help="Specifies whether a mail server or a name server query will be sent."
                                            "IP querry sent if neither are provided.")
     parser.add_argument("server", help="IPv4 server address in the format a.b.c.d")
-    parser.add_argument("domain", help="domain nam eto query for")
+    parser.add_argument("domain", help="domain name to query for")
 
 
 def initializeSocket(ip, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((ip, port))
     return s
-
-def generatePacketID():
-    headerID = bytearray()
-    for i in range(2):
-        r = random.randint(0, 255)
-        headerID.append(r)
-    return headerID
-
-def generateQueryFlags():
-    flags = bytearray()
-    flags.append(1)
-    flags.append(0)
-    return flags
-
-def generateQDCount():
-    qCount = bytearray()
-    qCount.append(0)
-    qCount.append(1)
-    return qCount
-
-def generateANCount():
-    aCount = bytearray()
-    aCount.append(0)
-    aCount.append(0)
-    return aCount
-
-def generateNSCount():
-    nCount = bytearray()
-    nCount.append(0)
-    nCount.append(0)
-    return nCount
-
-def generateARCount():
-    aCount = bytearray()
-    aCount.append(0)
-    aCount.append(0)
-    return aCount
-
-def generateNRCount():
-    nrCount = bytearray()
-    nrCount.append(0)
-    nrCount.append(0)
-    return nrCount
-
-# Generates  and returns a formatted DNS request Header.
-def generateDNSHeader():
-    header = bytearray()
-    header.append(generatePacketID())
-    header.extend(generateQueryFlags()) # For us it should always be 1, and 0
-    header.extend(generateQDCount()) # QDCount is always 1 for us
-    header.extend(generateANCount()) # Default 0, only regarded in response packets
-    header.extend(generateNSCount())
-    header.extend(generateNRCount())
-    return header
-
-def generateQName(name):
-    qName = bytearray()
-    tokens = name.split('.')
-    for token in tokens:
-        token = token.replace(" ", "")
-        qName.append(len(token))
-        for char in token:
-            qName.append(char)
-    qName.append(0)
-    return qName
-
-def generateQType():
-    qType = bytearray() #needs to choose between 1, 2, and 3. for now use 1 since default ip type.
-    qType.append(0)
-    qType.append(1)
-    return qType
-
-def generateQClass():
-    qClass = bytearray()
-    qClass.append(0)
-    qClass.append(1)
-    return qClass
-
-def generateDNSQuestions(name):
-    questions = bytearray()
-    questions.extend(generateQName(name))
-    return questions
-
-def sendDNSRequest():
-    print("Sent")
 
 
 # Press the green button in the gutter to run the script.
@@ -121,13 +37,12 @@ if __name__ == '__main__':
     args = parser.parse_args()
     printArgs()
 
+    packet = DNS.generateDNSHeader()
+    packet.extend(DNS.generateDNSQuestions(args.domain))
+
     serverIP = args.server.replace("@", "")
     sock = initializeSocket(serverIP, args.port)  # Initializes a socket object and connects to the server.
 
-    packet = generateDNSHeader()
-    packet.extend(generateDNSQuestions(args.domain))
-    packet.extend(generateQType())
-    packet.extend(generateQClass())
 
 
 
